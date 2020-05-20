96b09070b2a2f1c9ffcb3c1242d1f4a517a283a0
Floch <gaelle_floch@hotmail.com>
2020-05-20 15:42:11 +0200

    easy admin composer

--- src/Security/UsersAuthenticator.php
+++ src/Security/UsersAuthenticator.php
@@ -97,11 +97,13 @@ class UsersAuthenticator extends AbstractFormLoginAuthenticator implements Passw
         }
 
         // For example : return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+        //throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+        return new RedirectResponse($this->urlGenerator->generate('homepage'));
     }
 
     protected function getLoginUrl()
     {
-        return $this->urlGenerator->generate(self::LOGIN_ROUTE);
+        //return $this->urlGenerator->generate(self::LOGIN_ROUTE);
+        return $this->urlGenerator->generate('app_login');
     }
 }
